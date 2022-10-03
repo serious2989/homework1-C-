503523a9baea35a307aae8a02f8342458fb43373
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-03 11:06:40 +0300

    чётные числа от 1 до N

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите длину: ");
+int n = Convert.ToInt32(Console.ReadLine());
+    
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
